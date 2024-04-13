7a4bf93e044548d1b7680b6cb940d3ec5ba9e8fd
Nazarena Araceli <119014598+Nazgonzalezz@users.noreply.github.com>
2024-04-13 19:12:32 -0300

    Update main.c

--- PROYECTO_FECHA/main.c
+++ PROYECTO_FECHA/main.c
@@ -1,18 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "fecha.h"
 int main()
 {
     T_FECHA fecha1,fecha2;
+    int dife;
 
     puts("\n PRIMERA FECHA FECHA");
     fecha1=IngresarFecha(fecha1);
-    MostrarFecha(fecha1);
 
     puts("\n SEGUNDA FECHA");
     fecha2=IngresarFecha(fecha2);
-    MostrarFecha(fecha2);
 
-
-
+    dife=DiferenciaEntreFechas(&fecha1,&fecha2);
+    printf("\n\La diferencia entre las fechas es: %d",dife);
 
     return 0;
 }
