f3907f68119ff23bd3601fdc7721a7b80dd4b26a
Nazarena Araceli <119014598+Nazgonzalezz@users.noreply.github.com>
2024-05-08 13:04:27 -0300

    Add files via upload

---
+++ TP_20241c_viernes/TP_20241c_Viernes/bmpmanipuleitor/funciones_estudiante.c
@@ -0,0 +1,39 @@
+/*
+    Integrantes del grupo.
+    Si alguno de los integrantes del grupo dejara la materia, completar de todos modos sus datos, aclarando que no entrega.
+    -----------------
+    Apellido:
+    Nombre:
+    DNI:
+    Entrega:
+    -----------------
+    Apellido:
+    Nombre:
+    DNI:
+    Entrega:
+    -----------------
+    Apellido:
+    Nombre:
+    DNI:
+    Entrega:
+    -----------------
+    Apellido:
+    Nombre:
+    DNI:
+    Entrega:
+    -----------------
+
+    Comentarios (opcionales) que deseen hacer al docente sobre el TP:
+
+*/
+
+#include "constantes.h"
+
+int solucion(int argc, char* argv[])
+{
+/*
+    Aquí deben hacer el código que solucione lo solicitado.
+    Todas las funciones utilizadas deben estar declaradas en este archivo, y en su respectivo .h
+*/
+    return TODO_OK;
+}
