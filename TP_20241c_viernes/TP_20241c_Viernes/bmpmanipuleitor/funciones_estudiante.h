de4fa081454d3027db19ca5b4625b90803221968
Nazarena Araceli <119014598+Nazgonzalezz@users.noreply.github.com>
2024-05-19 17:33:22 -0300

    Update funciones_estudiante.h

--- TP_20241c_viernes/TP_20241c_Viernes/bmpmanipuleitor/funciones_estudiante.h
+++ TP_20241c_viernes/TP_20241c_Viernes/bmpmanipuleitor/funciones_estudiante.h
@@ -1,15 +1,21 @@
 #ifndef FUNCIONES_ESTUDIANTE_H_INCLUDED
 #define FUNCIONES_ESTUDIANTE_H_INCLUDED
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include "constantes.h"
-
+#include "estructuras.h"
 int solucion(int argc, char* argv[]);
-void _CopiarImagen (char nombImagen[]);
-void _tonalidadAzul();
-void _tonalidadRojo();
-void _tonalidadVerde();
-void _reducirContraste();
-void _escalaDeGrises();
-void _aumentarContraste();
+
+void _escalaDeGrises(char argv[]);
+void _aumentarContraste( char argv[]);
+void _CopiarImagen (const char nombImagen[],char argv[]);
+void _tonalidadAzul(char argv[]);
+void _tonalidadRojo(char argv[]);
+void _tonalidadVerde(char argv[]);
+void _reducirContraste(char argv[]);
+
 
 #endif // FUNCIONES_ESTUDIANTE_H_INCLUDED
